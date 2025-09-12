7ebbfa4312a70a52b5d660260e451ed12d40ab85
franpsS2 <francianepereira4545@gmail.com>
2025-09-12 11:02:36 -0300

    12/09/2025

--- ifmais_react/src/App.tsx
+++ ifmais_react/src/App.tsx
@@ -1,11 +1,16 @@
 
+// Imports CSS
 import './App.css'
+
+// Shadcn Components
+import { Button } from "@/components/ui/button"
 
 const App = () => {
 
   return (
-    <div className="app">
-      <h1 className='text-3xl font-bold underline'> My App </h1>
+    <div className="app flex flex-col items-center justify-center">
+      <h1 className='text-3xl font-bold underline mb-4'> My App </h1>
+      <Button> Finalizar Compra </Button>
     </div>
   );
 }
--- ifmais_react/vite.config.ts
+++ ifmais_react/vite.config.ts
@@ -1,6 +1,7 @@
+import path from "path"
+
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
-
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
@@ -9,4 +10,10 @@ export default defineConfig({
     react(),
     tailwindcss()
   ],
+
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
